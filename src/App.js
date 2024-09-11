bfc2e56eb28d2f8986fd53d2cc436f58c1479ff8
unknown <keith.francis83@gmail.com>
2024-09-10 20:25:12 -0600

    created navbar, setup routes for navbar and future shop page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import { Routes, Route } from 'react-router-dom';
 
+import Navbar from './routes/navbar/navbar.component';
 import Home from './routes/home/home.component';
+
+
 
 const App = () => {
   return (
     <Routes>
-      <Route path='/home' element={ <Home /> } />
+      <Route path='/' element={<Navbar />}>
+        <Route index element={ <Home /> } />
+        <Route path='shop' element={ <Shop /> } />
+      </Route> 
+      
     </Routes>
   )
 };
---
+++ src/routes/navbar/navbar.component.jsx
@@ -0,0 +1,25 @@
+import { Fragment } from "react";
+import { Outlet, Link } from "react-router-dom";
+
+import { ReactComponent as CrwnLogo } from '../../assets/crown.svg';
+import './navbar.styles.scss';
+
+const Navbar = () => {
+  return (
+    <Fragment>
+      <div className='navbar'>
+        <Link className='logo-container' to='/'>
+          <CrwnLogo className='logo' />
+        </Link>
+        <div className='nav-links-container'>
+          <Link className='nav-link' to='/shop'>
+            Shop
+          </Link>
+        </div>
+      </div>
+      <Outlet />
+    </Fragment>
+  )
+}
+
+export default Navbar;
