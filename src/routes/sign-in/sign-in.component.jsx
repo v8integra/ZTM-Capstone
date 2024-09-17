cf865438aa7d4c9df56bcd2fa5aae0824b873819
unknown <keith.francis83@gmail.com>
2024-09-16 20:43:47 -0600

    removing sign in with redirect

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,21 +1,10 @@
-import { useEffect } from 'react';
-import { getRedirectResult } from 'firebase/auth';
-
 import {
   auth,
-  signInWithGooglePopup, 
-  signInWithGoogleRedirct, 
+  signInWithGooglePopup,
   createUserDocumentFromAuth 
 } from '../../utils/firebase/firsebase.utils'
 
 const SignIn = () => {
-  useEffect(async() => {
-    const res = await getRedirectResult(auth);
-    if(res) {
-      const userDocRef = await createUserDocumentFromAuth(res.user);
-    }
-  }, []);
-
   const logGooglePopupUser = async() => {
     const { user }= await signInWithGooglePopup();
     createUserDocumentFromAuth(user);
@@ -26,9 +15,6 @@ const SignIn = () => {
       <h1>Sign In</h1>
       <button onClick={logGooglePopupUser}>
         Sign in with Google Popup
-      </button>
-      <button onClick={signInWithGoogleRedirct}>
-        Sign in with Google Redirect
       </button>
     </div>
   )
